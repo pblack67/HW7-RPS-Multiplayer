ead6460e7a2e326214111ed8e671eb317e9db4ae
Paul Black <pblack67@comcast.net>
2019-04-21 11:43:37 -0500

    Initial setup

---
+++ assets/javascript/rps.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDipImY3E3D3wQGifx2_x8bP-k92bdfWUc",
+    authDomain: "fir-sandbox-14b6e.firebaseapp.com",
+    databaseURL: "https://fir-sandbox-14b6e.firebaseio.com",
+    projectId: "fir-sandbox-14b6e",
+    storageBucket: "fir-sandbox-14b6e.appspot.com",
+    messagingSenderId: "407143467438"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
