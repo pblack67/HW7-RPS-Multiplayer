22d04d49f6880e59a9925508bdecc0931672868a
Paul Black <pblack67@comcast.net>
2019-04-22 10:04:09 -0500

    Added standings and victory processing

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,4 +1,3 @@
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDipImY3E3D3wQGifx2_x8bP-k92bdfWUc",
@@ -15,10 +14,17 @@ var database = firebase.database();
 var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
 var rpsDataRef = database.ref("/rpsData");
+
+// Global Variables
 var userName = "";
 var loggedIn = false;
 var choices = [];
 var myChoice = "";
+
+// Standings
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
 function newConnectionCallback(snap) {
     console.log("newConnectionCallback");
@@ -54,13 +60,64 @@ function saveChoices() {
     );
 }
 
+function updateStandings() {
+    console.log("Wins:", wins);
+    console.log("Losses:", losses);
+    console.log("Ties:", ties);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#ties").text(ties);
+}
+
+function isWinner(choice1, choice2) {
+    if ((choice1 === "rock") && (choice2 === "scissors") ||
+        (choice1 === "scissors") && (choice2 === "paper") ||
+        (choice1 === "paper") && (choice2 === "rock")) {
+        return true;
+    }
+
+    return false;
+}
+
+function setWinLossMessage(rps1, rps2, isWin) {
+    var winLossMessage = "";
+    if (isWin) {
+        winLossMessage = rps1 + " beats " + rps2 + ". You Win!!!";
+    } else {
+        winLossMessage = rps2 + " beats " + rps1 + ". You Lose!!! Bummer, dude.";
+    }
+    $("#winloss").text(winLossMessage);
+}
+
 function rpsDataCallback(snapshot) {
     console.log("rpsDataCallback called");
     console.log("snapshot.val()", snapshot.val());
     if (snapshot.val() != null) {
         choices = snapshot.val().choices;
         if (choices.length == 2) {
-            console.log("Choose winner here", choices);
+            var opponentChoice = "";
+            if (choices[0] === myChoice) {
+                opponentChoice = choices[1];
+            } else {
+                opponentChoice = choices[0];
+            }
+            console.log("myChoice", myChoice);
+            console.log("opponentChoice", opponentChoice);
+            if (myChoice == opponentChoice) {
+                $("#winloss").text("You both chose " + myChoice + ". It's a tie!");
+                ties++;
+            } else {
+                var result = isWinner(myChoice, opponentChoice);
+                setWinLossMessage(myChoice, opponentChoice, result);
+                if (result) {
+                    wins++;
+                } else {
+                    losses++;
+                }
+            }
+
+            updateStandings();
+
             choices = [];
             myChoice = "";
             saveChoices();
